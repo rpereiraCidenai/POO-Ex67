8f11e8f4dae9cc5d78b453287debd843c0d3d0b2
rpereiraCidenai <148018251+rpereiraCidenai@users.noreply.github.com>
2024-11-25 16:47:33 +0000

    poo triangulo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace POO_Ex67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação de uma instância da classe Triangulo
+            Triangulo triangulo = new Triangulo();
+
+            // Solicitar ao utilizador o comprimento dos lados do triângulo
+            Console.WriteLine("Insira o comprimento do lado A:");
+            double ladoA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o comprimento do lado B:");
+            double ladoB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o comprimento do lado C:");
+            double ladoC = Convert.ToDouble(Console.ReadLine());
+
+            // Definir as dimensões do triângulo com os valores fornecidos
+            triangulo.Dimensoes(ladoA, ladoB, ladoC);
+
+            // Guardar o tipo de triângulo numa variável antes de exibir
+            string tipoTriangulo = triangulo.Tipo();
+
+            // Exibir o tipo de triângulo
+            Console.WriteLine("O triângulo com lados A = " + ladoA + ", B = " + ladoB + ", e C = " + ladoC + " é: " + tipoTriangulo);
+            //saasd
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Ex67
+{
+    internal class Triangulo
+    {
+        // Atributos privados que representam os lados do triângulo
+        private double ladoA;
+        private double ladoB;
+        private double ladoC;
+
+        // Construtor vazio
+        public Triangulo() { }
+
+        // Método para definir as dimensões dos lados do triângulo
+        public void Dimensoes(double A, double B, double C)
+        {
+            ladoA = A;
+            ladoB = B;
+            ladoC = C;
+        }
+
+        // Método para identificar o tipo de triângulo
+        public string Tipo()
+        {
+            if (ladoA == ladoB && ladoB == ladoC)
+            {
+                return "Equilátero";
+            }
+            else if (ladoA != ladoB && ladoB != ladoC && ladoA != ladoC)
+            {
+                return "Escaleno";
+            }
+            else
+            {
+                return "Isósceles";
+            }
+        }
+    }
+}
